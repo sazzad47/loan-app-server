e7ddad863dca844eb558596cc13afba939bebab6
Ambrose <ambrosetall@gmail.com>
2023-01-25 17:42:17 +0300

    changing routes

--- frontend/src/state/api/api.js
+++ frontend/src/state/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:5000";
+// export const API_URL = "http://localhost:5000";
+export const API_URL = "http://74.63.223.224:5000";
 
 const api = axios.create({
   headers: {
