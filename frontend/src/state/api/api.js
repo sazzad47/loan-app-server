eb7a84ce644a498ff294377639902395786ea9d2
Ambrose <85063229+AmbroTall@users.noreply.github.com>
2023-09-06 00:47:25 +0300

    Update api.js

--- frontend/src/state/api/api.js
+++ frontend/src/state/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:4000";
+// export const API_URL = "http://localhost:4000";
+export const BASE_URL = "http://client1.jewelercart.com:4000";
 
 const api = axios.create({
   headers: {
